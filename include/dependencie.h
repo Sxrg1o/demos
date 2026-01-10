e19adffb0452235ab49ab3279dec006fd09e15b6
nhrot-fc <a20213812@pucp.edu.pe>
2026-01-10 16:13:29 -0500

    Update .gitignore and add dependencie.h file

---
+++ include/dependencie.h
@@ -0,0 +1,10 @@
+#ifndef DEPENDENCIE_H
+#define DEPENDENCIE_H
+
+/* 
+    TODO: Hacer que esto sea un import obligatorio
+    Si no se importa no se compila nada jajajajajasjsjsjs
+*/
+const int VERY_IMPORTANT_VARIABLE = 0;
+
+#endif // DEPENDENCIE_H
