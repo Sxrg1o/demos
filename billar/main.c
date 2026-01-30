60ded8402a5b68decb67d53cccf18ed2a2558bec
Bryan-Salvatierra <bryan.salvatierra01@gmail.com>
2026-01-30 01:14:45 -0500

    billar

--- billar/main.c
+++ billar/main.c
@@ -4,21 +4,22 @@
 
 
 #define SQUARE_SIZE 31
-#define STOP_EPS 0.01
+#define STOP_EPS 5
+#define elasticity 1.0f
 
 typedef struct Ball{
     Vector2 position;
     int size;
     Vector2 speed;
     Color color;
+    
 }Ball;
 
 
 const int screenWidth  = 1800;
 const int screenHeight = 1500;
-const float acel_x = 0;
-const float acel_y = 0;
-const Vector2 acel = {acel_x , acel_y};
+const Vector2 acel = {0 , 0};
+
 float position_relative(float speed, float aceleration, float position, float frame){
     return position + ( (speed + (0.5f) * aceleration * frame )* frame);
 }
@@ -29,6 +30,30 @@ float speed_relative(float speed, float aceleration, float frame){
 void Stop(Ball *ball){
     if(fabs(ball->speed.x) < STOP_EPS) ball->speed.x = 0.0f;
     if(fabs(ball->speed.y) < STOP_EPS) ball->speed.y = 0.0f;
+}
+
+void ColitionBalls(Ball *ball_1,Ball *ball_2){
+    float dif_x = ball_2->position.x - ball_1->position.x;
+    float dif_y = ball_2->position.y - ball_1->position.y;
+    
+    float norm_dif = sqrt(dif_x * dif_x + dif_y * dif_y );
+
+    Vector2 normal_dif = {dif_x/norm_dif, dif_y/norm_dif};
+    
+    float v_nomal = ball_1->size + ball_2->size;
+    float impulse = -((1 + elasticity)/2) * v_nomal;
+
+    ball_1->speed.x = ball_1->speed.x - impulse * normal_dif.x;
+    ball_1->speed.y = ball_1->speed.y - impulse * normal_dif.y;
+    ball_2->speed.x = ball_2->speed.x - impulse * normal_dif.x;
+    ball_2->speed.y = ball_2->speed.y - impulse * normal_dif.y;
+}
+
+void Friction(Ball *ball, float frime){
+    const float k = 0.1f;
+    float factor = expf(-k * frime);
+    ball->speed.x *= factor;
+    ball->speed.y *= factor;
 }
 
 void StateUpdate(Ball *ball, float frame){
@@ -69,23 +94,39 @@ int main(void)
     ball_1.color = BLACK;
     ball_1.position = (Vector2) {(float)screenWidth/2, (float)screenHeight/2};
     ball_1.size = 50;
-    ball_1.speed = (Vector2) {0, 1};
+    ball_1.speed = (Vector2) {400, 200};
+    
+
+    Ball ball_2;
+    ball_2.color = BLACK;
+    ball_2.position = (Vector2) {(float)screenWidth/4, (float)screenHeight/4};
+    ball_2.size = 50;
+    ball_2.speed = (Vector2) {800, 1000};
+    
 
 
     InitWindow(screenWidth, screenHeight, "Billiards");
     while (!WindowShouldClose())
     {
-        float time = GetFrameTime() * 0.6f;
+        float time = GetFrameTime()*0.6f;
 
-        for (int i = 0; i < 10; i++){
+        for(int i = 0; i < 10; i++){
             StateUpdate(&ball_1, time);
-            }
-        StateLogic(&ball_1);
+            StateUpdate(&ball_2, time);
+            //ColitionBalls(&ball_1, &ball_2);
+            Friction(&ball_1,  time);
+            Friction(&ball_2, time);
+            StateLogic(&ball_1);
+            StateLogic(&ball_2);
+
+        }
 
         BeginDrawing();
     
         ClearBackground(RAYWHITE);
         DrawCircle(ball_1.position.x, ball_1.position.y, ball_1.size, ball_1.color);
+        DrawCircle(ball_2.position.x, ball_2.position.y, ball_2.size, ball_2.color);
+
         DrawText(
     TextFormat("speed: (%.2f, %.2f)", ball_1.speed.x, ball_1.speed.y),
     10, 10, 20, BLACK
@@ -96,6 +137,7 @@ int main(void)
     CloseWindow();
     return 0;
 }
+
 
 
 
